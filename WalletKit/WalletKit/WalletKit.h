f0af3914494d92aa7a7dd96192019c3c3cfd4f57
yuzushioh <yuzushioh@gmail.com>
2018-01-01 17:00:27 +0900

    initial commit

---
+++ WalletKit/WalletKit/WalletKit.h
@@ -0,0 +1,19 @@
+//
+//  WalletKit.h
+//  WalletKit
+//
+//  Created by yuzushioh on 2018/01/01.
+//  Copyright © 2018 yuzushioh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WalletKit.
+FOUNDATION_EXPORT double WalletKitVersionNumber;
+
+//! Project version string for WalletKit.
+FOUNDATION_EXPORT const unsigned char WalletKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalletKit/PublicHeader.h>
+
+
