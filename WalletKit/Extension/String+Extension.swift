7d69a0568ed0fabbefc322f54b4786ade08acb85
yuzushioh <yuzushioh@gmail.com>
2018-01-24 20:56:13 +0900

    add base 58 encoding extension to string

--- WalletKit/Extension/String+Extension.swift
+++ WalletKit/Extension/String+Extension.swift
@@ -31,3 +31,32 @@ extension String {
         return data(using: String.Encoding.utf8, allowLossyConversion: true)?.bytes ?? []
     }
 }
+
+// MARK: Base58Encoding
+extension String {
+    var base58EncodingString: String {
+        guard var big = BInt(self) else {
+            fatalError("Could not initialize BigInt with value of \(self)")
+        }
+        
+        let base58EncodeCharacters: [Character] = [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F", "G", "H", "J",
+            "K", "L", "M", "N", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i",
+            "j", "k", "m", "n", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z"
+        ]
+        
+        var base58encodedString = ""
+        while big > 0 {
+            let bigRemainder  = big % 58
+            let remainder = Int(bigRemainder.description)
+            base58encodedString = String("\(base58EncodeCharacters[remainder!])\(base58encodedString)")
+            big = big / 58
+        }
+        
+        return base58encodedString
+    }
+}
