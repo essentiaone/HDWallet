67bb4f0cba905a94e611b9b68ce17966b10290a6
yuzushioh <yuzushioh@gmail.com>
2018-02-11 20:05:04 +0900

    add carthage build

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -20,12 +20,10 @@ class ViewController: UIViewController {
         // abandon amount liar amount expire adjust cage candy arch gather drum buyer
         
         let seed = MnemonicGenerator.createSeed(mnemonic: mnemonic)
-        print(seed)
         // 3779b041fab425e9c0fd55846b2a03e9a388fb12784067bd8ebdb464c2574a05bcc7a8eb54d7b2a2c8420ff60f630722ea5132d28605dbc996c8ca7d7a8311c0
         
         // By calling initialize, you can now access to your wallet.
-        let wallet = Wallet(seed: seed, network: .test)
-        print(wallet.privateKey.extended, wallet.publicKey.extended)
+        let wallet = Wallet(seed: seed, network: .main)
         
         // Master private key and master public key generated from the seed.
         // tprv8ZgxMBicQKsPdM3GJUGqaS67XFjHNqUC8upXBhNb7UXqyKdLCj6HnTfqrjoEo6x89neRY2DzmKXhjWbAkxYvnb1U7vf4cF4qDicyb7Y2mNa
