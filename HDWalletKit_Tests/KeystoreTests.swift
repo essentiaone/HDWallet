0eac476bce9d6c261a1cf0b1dc6fc4f79f13c287
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-09-07 17:00:54 +0300

    Fix keystore json encoding

--- HDWalletKit_Tests/KeystoreTests.swift
+++ HDWalletKit_Tests/KeystoreTests.swift
@@ -13,7 +13,9 @@ class KeystoreTests: XCTestCase {
     
     func testKeyStoreGeneration() {
         let data = Data(hex: "576896308291575cb5b61eacd178e19721736b63594ee4c9bf60ffd9f3bd869de2124580d9ef0d4446fe467a7014b9894a738780db3c00cde5f86a7922115bdb")
-        let keystore = try! KeystoreV3(seed: data, password: "bYSqu6{X")
+        let keystore = try! KeystoreV3(seed: data, password: "qwertyui")
+        let encodedData = (try? keystore?.encodedData())!
+        print((encodedData! as NSData).description)
         XCTAssertEqual(keystore?.keystoreParams?.crypto.cipher, "aes-128-ctr")
         XCTAssertEqual(keystore?.keystoreParams?.crypto.kdf, "scrypt")
         XCTAssertEqual(keystore?.keystoreParams?.crypto.kdfparams.r, 8)
@@ -24,7 +26,7 @@ class KeystoreTests: XCTestCase {
     
     func testDecodeKeystore() {
         let data = Data(hex: "576896308291575cb5b61eacd178e19721736b63594ee4c9bf60ffd9f3bd869de2124580d9ef0d4446fe467a7014b9894a738780db3c00cde5f86a7922115bdb")
-        let password = "bYSqu6{X"
+        let password = "qwertyui"
         let keystore = try! KeystoreV3(seed: data, password: password)
         guard let decoded = try? keystore?.getDecriptedKeyStore(password: password) else {
             assertionFailure()
