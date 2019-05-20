3d5dc5600736dab7840ea278009f76039a15a471
impul <pavlo.bojkoo@gmail.com>
2019-05-20 17:54:08 +0300

    Fix Dash Pay to Script Hash address support

--- HDWalletKit/Wallet/UTXOBased/Bitcoin/BitcoinAddress.swift
+++ HDWalletKit/Wallet/UTXOBased/Bitcoin/BitcoinAddress.swift
@@ -55,12 +55,12 @@ public struct LegacyAddress: Address {
         let type: AddressType
         let addressPrefix = pubKeyHash[0]
         switch addressPrefix {
-        case coin.scriptHash:
-            type = .scriptHash
         case coin.publicKeyHash:
             type = .pubkeyHash
         case coin.wifAddressPrefix:
             type = .wif
+        case coin.scriptHash:
+            type = .scriptHash
         default:
             throw AddressError.invalidVersionByte
         }
